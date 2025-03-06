2d18fa4b6d39216307a606d970864c70cea4298a
lz109 <linzhan@andrew.cmu.edu>
2025-03-06 15:01:29 -0500

    Fix STATIC_ROOT issue for Heroku

--- webapps/settings.py
+++ webapps/settings.py
@@ -134,9 +134,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+# Enable WhiteNoise for serving static files
+INSTALLED_APPS += ['whitenoise.runserver_nostatic']
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
