007cd57304f50df2bae1228ad3772a4d4a6b984e
lz109 <linzhan@andrew.cmu.edu>
2025-03-06 02:30:21 -0500

    change setting

--- webapps/settings.py
+++ webapps/settings.py
@@ -79,12 +79,6 @@ WSGI_APPLICATION = "webapps.wsgi.application"
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
-#     },
-#     'users': {
-#         'NAME': 'mydatabase',
-#         'ENGINE': 'django.db.backends.mysql',
-#         'USER': 'root',
-#         'PASSWORD': 'LynnZh_011019',
 #     }
 # }
 
