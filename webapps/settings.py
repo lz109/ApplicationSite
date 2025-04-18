71d5e542cfa35619721004a1e89f4aba13785e90
lz109 <linzhan@andrew.cmu.edu>
2025-04-18 14:48:19 -0400

    modified home page to make it prettier

--- webapps/settings.py
+++ webapps/settings.py
@@ -153,7 +153,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Enable WhiteNoise for serving static files
 INSTALLED_APPS += ['whitenoise.runserver_nostatic']
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
